86fc456c5794c3399c34eac2f911f62b4d4bed30
Amirreza Jaberi <83431690+AmIrReZa386@users.noreply.github.com>
2022-11-09 18:12:58 +0330

    Add `UnBan` table for unban command access

--- whitelists/fire-white.lua
+++ whitelists/fire-white.lua
@@ -17,3 +17,10 @@ AdminMenu = {
     '192.168.1.**',                                      -- IP Address
     'license2:500a67097ce3c274569c773bc41974d3c28380**', -- FiveM License
 }
+
+UnBan = {
+    '7835767148521717**',                                -- Discord ID
+    'steam:1100001476d05**',                             -- Steam Hex
+    '192.168.1.**',                                      -- IP Address
+    'license2:500a67097ce3c274569c773bc41974d3c28380**', -- FiveM License
+}
