b6d318a3287b2a950bb4776b9bae08e59506ba6b
Amirreza Jaberi <amirrezajabery112@gmail.com>
2023-12-01 21:32:35 +0330

    Added webhooks to `fire-webhook.lua`

---
+++ configs/fire-webhook.lua
@@ -0,0 +1,14 @@
+--
+-- FIREAC (https://github.com/AmirrezaJaberi/FIREAC)
+-- Copyright 2022-2023 by Amirreza Jaberi (https://github.com/AmirrezaJaberi)
+-- Licensed under the GNU Affero General Public License v3.0
+--
+
+FIREAC.Webhooks = {
+    Ban        = "",
+    Error      = "",
+    Connect    = "",
+    Disconnect = "",
+    Exoplosion = "",
+    ScreenShot = "",
+}
