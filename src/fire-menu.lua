fdef388f234773b364353863275657b822581de8
Amirreza Jaberi <amirrezajabery112@gmail.com>
2023-12-16 18:45:56 +0330

    Added base of key and command for admin menu

--- src/fire-menu.lua
+++ src/fire-menu.lua
@@ -3,3 +3,14 @@
 -- Copyright 2022-2023 by Amirreza Jaberi (https://github.com/AmirrezaJaberi)
 -- Licensed under the GNU Affero General Public License v3.0
 --
+
+local isAdmin = false
+
+
+RegisterCommand('fireacmenu', function()
+    if isAdmin then
+
+    end
+end, false)
+
+RegisterKeyMapping("fireacmenu", "FIREAC admin menu", "keyboard", FIREAC.AdminMenu.Key)
