c9d7066b5b72ce0420599a00461f632c02d0ce7f
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-06 13:37:43 +0330

    Added comment for all SQL table insert

--- database.sql
+++ database.sql
@@ -1,9 +1,11 @@
+-- Table for storing admin information
 CREATE TABLE IF NOT EXISTS `fireac_admin` (
   `id` int(11) NOT NULL DEFAULT 1,
   `identifier` longtext NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- Table for storing banned player information
 CREATE TABLE IF NOT EXISTS `fireac_banlist` (
   `id` int(11) NOT NULL DEFAULT 1,
   `STEAM` longtext NOT NULL,
@@ -18,12 +20,14 @@ CREATE TABLE IF NOT EXISTS `fireac_banlist` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- Table for storing unbanned player information
 CREATE TABLE IF NOT EXISTS `fireac_unban` (
   `id` int(11) NOT NULL DEFAULT 1,
   `identifier` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- Table for storing whitelisted player information
 CREATE TABLE IF NOT EXISTS `fireac_whitelist` (
   `id` int(11) NOT NULL DEFAULT 1,
   `identifier` longtext NOT NULL,
