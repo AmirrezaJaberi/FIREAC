5cb122c2cbc255d74e3293b6ca5888b3abaab827
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-01-06 13:45:37 +0330

    Updated webhook file & Add comment

--- configs/fire-webhook.lua
+++ configs/fire-webhook.lua
@@ -1,14 +1,27 @@
---
 -- FIREAC (https://github.com/AmirrezaJaberi/FIREAC)
--- Copyright 2022-2023 by Amirreza Jaberi (https://github.com/AmirrezaJaberi)
+-- Copyright 2022-2024 by Amirreza Jaberi (https://github.com/AmirrezaJaberi)
 -- Licensed under the GNU Affero General Public License v3.0
 --
 
+-- Webhooks Configuration:
+-- Add your Discord webhooks below to receive logs and notifications.
+
 FIREAC.Webhooks = {
+    -- Ban Webhook: Receive ban-related logs.
     Ban        = "",
+
+    -- Error Webhook: Receive error logs.
     Error      = "",
+
+    -- Connect Webhook: Receive connection-related logs.
     Connect    = "",
+
+    -- Disconnect Webhook: Receive disconnection-related logs.
     Disconnect = "",
-    Exoplosion = "",
+
+    -- Explosion Webhook: Receive explosion-related logs.
+    Explosion  = "",
+
+    -- Screenshot Webhook: Receive screenshot-related logs.
     ScreenShot = "",
 }
