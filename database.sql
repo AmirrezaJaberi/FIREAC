03ab4963d100257951fb33efd5bc9b3608647e93
Amirreza Jaberi <amirrezajabery112@gmail.com>
2024-08-29 19:24:05 +0330

    Fixed database problem (save all id as `1`)

--- database.sql
+++ database.sql
@@ -1,13 +1,13 @@
 -- Table for storing admin information
 CREATE TABLE IF NOT EXISTS `fireac_admin` (
-  `id` int(11) NOT NULL DEFAULT 1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` longtext NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Table for storing banned player information
 CREATE TABLE IF NOT EXISTS `fireac_banlist` (
-  `id` int(11) NOT NULL DEFAULT 1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `STEAM` longtext NOT NULL,
   `DISCORD` longtext NOT NULL,
   `LICENSE` longtext NOT NULL,
@@ -22,14 +22,14 @@ CREATE TABLE IF NOT EXISTS `fireac_banlist` (
 
 -- Table for storing unbanned player information
 CREATE TABLE IF NOT EXISTS `fireac_unban` (
-  `id` int(11) NOT NULL DEFAULT 1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Table for storing whitelisted player information
 CREATE TABLE IF NOT EXISTS `fireac_whitelist` (
-  `id` int(11) NOT NULL DEFAULT 1,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `identifier` longtext NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
